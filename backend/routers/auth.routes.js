4845292bbef6e87e233345d792c5a90fbcaab94e
HsiaoCz <984274788@qq.com>
2024-05-03 06:50:25 +0800

    some

--- backend/routers/auth.routes.js
+++ backend/routers/auth.routes.js
@@ -1,23 +1,12 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.json({
-    data: "You hit the signup endpoint",
-  });
-});
+router.post("/signup", signup);
 
-router.get("/login", (req, res) => {
-  res.json({
-    data: "this is login",
-  });
-});
+router.post("/login", login);
 
-router.get("/logout", (req, res) => {
-  res.json({
-    data: "this is logout",
-  });
-});
+router.get("/logout", logout);
 
 export default router;
